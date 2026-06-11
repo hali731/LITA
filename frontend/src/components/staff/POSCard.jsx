8aeb4dff5625232a34bf3d750122f29d4b8435dd
Soulita <admin@gmail.com>
2026-06-12 03:33:02 +0700

    UI: visually indicate out-of-stock items in POS/Customer views (grayscale image, disable add button)

--- frontend/src/components/staff/POSCard.jsx
+++ frontend/src/components/staff/POSCard.jsx
@@ -2,15 +2,22 @@ import { FiPlus } from "react-icons/fi";
 
 export default function POSCard({ item, onAdd }) {
   return (
-    <div className="menu-card">
+    <div className={`menu-card ${item.stockQuantity <= 0 ? "out-stock" : ""}`}>
       <img src={item.image} alt={item.name} />
 
       <div className="menu-info">
         <h4>{item.name}</h4>
         <p>{item.price.toLocaleString()} đ</p>
+        {item.stockQuantity <= 0 && (
+          <span style={{ color: "red", fontSize: "12px", fontWeight: "bold" }}>Hết hàng</span>
+        )}
       </div>
 
-      <button className="add-btn" onClick={() => onAdd(item)}>
+      <button 
+        className="add-btn" 
+        onClick={() => onAdd(item)}
+        disabled={item.stockQuantity <= 0}
+      >
         <FiPlus />
       </button>
     </div>
